6579290b3810769d4fe04ae42d51a9e9da5c1042
soshi04 <sirsohumjoshi@gmail.com>
2025-05-22 14:42:39 -0500

    more modifications to navbar design

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,23 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import "./NavBar.css"
 
 type NavBarProps = {
-    currmode : number;
-    toggleMode : () => void;
+  currmode: number;
+  toggleMode: () => void;
+};
+
+
+function NavBar({ currmode, toggleMode }: NavBarProps) {
+  const [count, setCount] = useState(0)
+
+const CountHandle = () => {
+  setCount(count + 1)
 }
 
-function NavBar({ currmode, toggleMode }: NavBarProps){
-    return (
-        <nav className="navbar">
-          <div className="nav-links">
-            <Link to="/">Home</Link>
-            <Link to="/about">About Me</Link>
-          </div>
-          <button onClick={toggleMode}>
-            Mode: {currmode === 0 ? "Dark" : currmode === 1 ? "Light" : "Cream"}
-          </button>
-        </nav>
-      );
+  return (
+    <nav className="navbar">
+      <div className="nav-content">
+        <div className="nav-links">
+          <Link to="/">Home</Link>
+          <Link to="/about">About Me</Link>
+        </div>
+        <button onClick={toggleMode}>
+          {currmode === 0 ? "Dark" : currmode === 1 ? "Light" : "Cream"}
+        </button>
+        <button onClick={CountHandle}>Click me {count}</button>
+      </div>
+    </nav>
+  );
 }
 
-export default NavBar
+export default NavBar;
