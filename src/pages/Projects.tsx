52cac11aabb317208b7efc1f4c14387227cd2f26
soshi04 <sirsohumjoshi@gmail.com>
2025-05-30 16:41:57 -0500

    first project

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useState } from "react";
+import { Link } from 'react-router-dom'
 
 
 function Projects() {
@@ -7,6 +8,8 @@ function Projects() {
     return(
         <div>
             <h1>Projects Page</h1>
+            <Link to='https://leetcode-suggest-and-track.vercel.app/'>Leetcode Suggest</Link>
+
         </div>
 
     )}
