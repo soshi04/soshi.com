0267c32243a3e76a84b84702eb73edfaa244c7e2
soshi04 <sirsohumjoshi@gmail.com>
2025-06-04 22:12:14 -0500

    mainpage gradient

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -3,22 +3,27 @@ import './MainPage.css';
 import { Typography } from '@mui/material';
 
 type MainPageProps = {
-    currmode : number
-}
+    currmode: number;
+};
 
-function MainPage( {currmode}: MainPageProps ) {
-    return(
+function MainPage({ currmode }: MainPageProps) {
+    return (
         <div className="main-page">
-            <Typography variant='h2' color={currmode === 0 ? common.white : common.black }>Welcome to Soshi.com</Typography>
-            {/* <h1 className={
-                currmode === 0 ? 'h1-dark' : 'h1-light'
-            }> Welcome to Soshi.com</h1> */}
-             <Typography variant='subtitle1' color={currmode === 0 ? common.white : common.black }>This is the home page of Soshi.com</Typography>
-             <Typography variant='subtitle1' color={currmode === 0 ? common.white : common.black }>Here you can find more about me and my work</Typography>
-             <Typography variant='subtitle1' color={currmode === 0 ? common.white : common.black }>You can also find my projects and my blog</Typography>
-             <Typography variant='subtitle1' color={currmode === 0 ? common.white : common.black }>If you have any questions, please feel free to contact me at <a href="mailto:sjoshi376@gatech.edu">sjoshi376@gatech.edu</a></Typography>
+            <div className="gradient-overlay"></div>
+            <Typography variant='h2' color={currmode === 0 ? common.white : common.black}>
+                Hey, I'm Sohum 👋
+            </Typography>
+            <Typography variant='subtitle1' color={currmode === 0 ? common.white : common.black}>
+                Incoming CS Master's @ Georgia Tech | Machine Learning Engineer | Full-stack Developer
+            </Typography>
+            <Typography variant='subtitle1' color={currmode === 0 ? common.white : common.black}>
+                I'm currently building <b>Object Detection</b> — a tool to help you detect objects in images using OpenCV.
+            </Typography>
+            <Typography variant='subtitle1' color={currmode === 0 ? common.white : common.black}>
+                Check out my <a href="/projects">projects</a>, read my <a href="/blog">thoughts</a>, or <a href="mailto:sjoshi376@gatech.edu">get in touch</a>.
+            </Typography>
         </div>
     );
 }
 
-export default MainPage
+export default MainPage;
