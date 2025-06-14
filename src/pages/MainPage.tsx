e9dcfbbd86e9bd37539ff6290f6480c87e52925d
soshi04 <sirsohumjoshi@gmail.com>
2025-06-13 22:05:25 -0400

    fixed main page links

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,26 +1,31 @@
 import { common } from '@mui/material/colors';
 import './MainPage.css';
 import { Typography } from '@mui/material';
+import { Link } from "react-router-dom";
 
 type MainPageProps = {
     currmode: number;
 };
 
 function MainPage({ currmode }: MainPageProps) {
+    const color = currmode === 0 ? common.white : common.black;
     return (
         <div className="main-page">
             <div className="gradient-overlay"></div>
-            <Typography variant='h2' color={currmode === 0 ? common.white : common.black}>
+            <Typography variant='h2' color={color}>
                 Hey, I'm Sohum 👋
             </Typography>
-            <Typography variant='subtitle1' color={currmode === 0 ? common.white : common.black}>
+            <Typography variant='subtitle1' color={color}>
                 Incoming CS Master's @ Georgia Tech | Machine Learning Engineer | Full-stack Developer
             </Typography>
-            <Typography variant='subtitle1' color={currmode === 0 ? common.white : common.black}>
+            <Typography variant='subtitle1' color={color}>
                 I'm currently building <b>Object Detection</b> — a tool to help you detect objects in images using OpenCV.
             </Typography>
-            <Typography variant='subtitle1' color={currmode === 0 ? common.white : common.black}>
-                Check out my <a href="/projects">projects</a>, read my <a href="/blog">thoughts</a>, or <a href="mailto:sjoshi376@gatech.edu">get in touch</a>.
+            <Typography variant='subtitle1' color={color}>
+                Check out my{' '}
+                <Link to="/projects" className="custom-link">projects</Link>, read my{' '}
+                <Link to="/blog" className='custom-link'>thoughts</Link>, or{' '}
+                <a href="mailto:sjoshi376@gatech.edu" className='custom-link'>get in touch</a>.
             </Typography>
         </div>
     );
