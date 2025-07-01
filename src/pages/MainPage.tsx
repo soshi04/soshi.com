a0a005aee0a9812004696253f6785f399ddc5209
soshi04 <sirsohumjoshi@gmail.com>
2025-06-30 20:59:57 -0400

    type writer and centering

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,7 +1,8 @@
 import { common } from '@mui/material/colors';
 import './MainPage.css';
-import { Typography } from '@mui/material';
+import { Typography, Box } from '@mui/material';
 import { Link } from "react-router-dom";
+import { Typewriter } from 'react-simple-typewriter';
 
 type MainPageProps = {
     currmode: number;
@@ -12,8 +13,25 @@ function MainPage({ currmode }: MainPageProps) {
     return (
         <div className="main-page">
             <div className="gradient-overlay"></div>
-            <Typography variant='h2' color={color}>
-                Hey, I'm Sohum
+            <Box
+            sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                justifyContent: 'center',
+                gap: 2,
+                position: 'relative',
+                zIndex: 1,
+            }}>
+            <Typography variant='h2' color={color} sx={{ textAlign: 'center', fontWeight: 700 }}>
+                <Typewriter
+                words={["Hey, I'm Sohum"]}
+                cursor
+                cursorStyle=''
+                typeSpeed={60}
+                deleteSpeed={50}
+                delaySpeed={1000}
+                />
             </Typography>
             <Typography variant='subtitle1' color={color}>
                 Incoming CS Master's @ Georgia Tech | Machine Learning Engineer | Full-stack Developer
@@ -27,6 +45,7 @@ function MainPage({ currmode }: MainPageProps) {
                 <Link to="/blog" className='custom-link'>thoughts</Link>, or{' '} get in touch at
             </Typography>
             <Typography variant='subtitle1' color={color}> sjoshi376 [at] gatech [dot] edu </Typography>
+            </Box>
         </div>
     );
 }
